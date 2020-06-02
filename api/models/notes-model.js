dcf8de4880ee7c363f3de3fa05ab45c6d520d390
Sakshi Lajurkar <56391148+SakshiLajurkar@users.noreply.github.com>
2020-06-02 12:20:07 +0530

    Delete notes-model.js

--- api/models/notes-model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const notesSchema = new Schema({
-    title: { type: String, required: true},
-    content: { type: String, required: true}
-});
-
-module.exports = mongoose.model('Note', notesSchema);
