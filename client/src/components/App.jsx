f92f0b09c3c8d1f21abeed03d4ce582c445083f3
Sakshi Lajurkar <56391148+SakshiLajurkar@users.noreply.github.com>
2020-06-13 03:16:22 +0530

    chrome ext requirements

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState} from "react";
 import Header from "./Header";
-
+import './App.css';
 import Note from "./Note";
 import Create from "./Create";
+
 
 function App() {
   const [notes, setNotes] = useState([]);
